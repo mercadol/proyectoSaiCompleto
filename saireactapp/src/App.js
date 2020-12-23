11b97919bc2cfb6814dc407ec3514fc566c1a57d
Leonardo Mercado <lamercadob1@gmail.com>
2020-12-23 17:16:48 -0500

    maquetacion basica implementada

--- saireactapp/src/App.js
+++ saireactapp/src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import './assets/css/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+        <div className="content">
+          <header>
+          <nav role="navigation">
+            <div id="menuToggle">
+              <input type="checkbox" />
+              <span></span>
+              <span></span>
+              <span></span>
+              <ul id="menu">
+                  <li><a href="home.html">Home</a></li>
+                  <li><a href="personas.html">Lista de Personas</a></li>
+                  <li><a href="error404.html">Error 404</a></li>
+                  <li><a href="formulario.html">Formularios</a></li>
+              </ul>
+              <h1>esta es mi maqueta</h1>
+            </div>
+          </nav>
       </header>
+      <footer id="footer">
+        <div className="center">
+          <p>
+              &copy; Leonardo Mercardo 2020
+          </p>
+        </div>
+      </footer>
     </div>
+  </div>
   );
 }
 
