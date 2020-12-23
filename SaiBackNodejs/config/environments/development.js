eb45254948d9798733c91dce8997c7d4a2fe96c6
Leonardo Mercado <lamercadob1@gmail.com>
2020-12-23 17:17:45 -0500

    cambios menores para producion back

---
+++ SaiBackNodejs/config/environments/development.js
@@ -0,0 +1,10 @@
+module.export ={
+  PORT: 5000,
+  DB:{
+    user:"postgres",
+    password:"mysecretpassword",
+    database: "school_dev",
+    host: "localhost",
+    dialect: "postgres"
+  }
+};
---
+++ SaiBackNodejs/config/environments/index.js
@@ -0,0 +1,4 @@
+
+
+const PRODUCTION= require("./production");const PRODUCTION= require("./production");const PRODUCTION= require("./production");
+const DEVELOPMENT= require("./development");const PRODUCTION= require("./production");const PRODUCTION= require("./production");
